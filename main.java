e357357f000db300c0a9162b4b2673804159ef59
minh <nhatminhd0811@gmail.com>
2025-08-01 16:45:27 +0700

    init commit

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("a");
-    }
-}
+
+
+        for(int i=0; i<5;i++) {
+           System.out.println(i);
+        }
+
+}    }
+
