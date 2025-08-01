30f0cc46ea84c30f9196c330245c5943b75cdc5d
minh <nhatminhd0811@gmail.com>
2025-08-01 16:39:30 +0700

    init commit

---
+++ test_run.java
@@ -0,0 +1,26 @@
+public class test_run {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("13");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("15");
+        System.out.println("16");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("61");
+        System.out.println("1");
+
+
+
+    }
+    
+}
